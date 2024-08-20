85816da44e344b9d49a4ff48b9bb483b422108ce
cj <ckswls1019@naver.com>
2024-08-20 17:00:56 +0900

    추가:exhibitions_member 관련 코드 추가
    1.dto와 entity 코드 추가

--- Back-end/src/exhibitions_member/dto/create-exhibitions_member.dto.ts
+++ Back-end/src/exhibitions_member/dto/create-exhibitions_member.dto.ts
@@ -1,1 +1,18 @@
-export class CreateExhibitionsMemberDto {}
+import { IsInt, IsNotEmpty, IsString, IsOptional } from 'class-validator';
+
+export class CreateExhibitionsMemberDto {
+    @IsInt()
+    exhibitions_id: number; // 외래 키
+
+    @IsString()
+    @IsNotEmpty() // 이름은 필수
+    name: string; // 이름
+
+    @IsOptional() // 닉네임은 선택적
+    @IsString()
+    nick_name?: string; // 닉네임
+
+    @IsString()
+    @IsNotEmpty() // 기수는 필수
+    generation: string; // 기수
+}
--- Back-end/src/exhibitions_member/entities/exhibitions_member.entity.ts
+++ Back-end/src/exhibitions_member/entities/exhibitions_member.entity.ts
@@ -1,1 +1,20 @@
-export class ExhibitionsMember {}
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Exhibition } from 'src/exhibitions/exhibition.entity'; // Exhibition 엔티티 경로에 맞게 수정
+
+@Entity()
+export class ExhibitionsMember {
+    @PrimaryGeneratedColumn() // 자동 증가 주 키
+    exhibition_member_id: number; // 주 키
+
+    @ManyToOne(() => Exhibition, exhibition => exhibition.exhibitionsMembers) // Exhibition과의 관계 설정
+    exhibitions: Exhibition; // 외래 키
+
+    @Column()
+    name: string; // 이름
+
+    @Column({ nullable: true }) // 닉네임은 선택적이므로 nullable 설정
+    nick_name?: string; // 닉네임
+
+    @Column()
+    generation: string; // 기수
+}
