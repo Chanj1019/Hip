5e33b8e9f1d85e50631ecd0972002dfc2bb2b560
jaeho kim <rlawogh1005@gmail.com>
2024-08-24 00:20:19 +0900

    feat: doc_name.entity.ts 작성

--- Back-end/src/doc_name/entities/doc_name.entity.ts
+++ Back-end/src/doc_name/entities/doc_name.entity.ts
@@ -1,1 +1,25 @@
-export class DocName {}
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
+import { Course } from '../../courses/entities/course.entity'
+import { CourseDoc } from '../../course_doc/entities/course_doc.entity'
+
+@Entity()
+export class DocName {
+    @PrimaryGeneratedColumn()
+    topic_id: number;
+
+    @Column()
+    course_id: number;
+
+    @Column()
+    pa_topic_id: number;
+
+    @Column()
+    file_path: string;
+
+    @ManyToOne(() => Course, (course) => course.docName, { onDelete: 'CASCADE'})
+    @JoinColumn({ name: 'course_id' })
+    course: Course;
+
+    @OneToMany(() => CourseDoc, (coursedoc) => coursedoc.docName, { cascade: true })
+    courseDoc: CourseDoc[];
+}
