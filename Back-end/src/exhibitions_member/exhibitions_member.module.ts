7913c8dd3df3439811c2a8db6b9cbd178770eb89
cj <ckswls1019@naver.com>
2024-08-25 23:09:14 +0900

    수정: exhibitions_member 모듈 코드수정
    1.typeorm모듈 임포트

--- Back-end/src/exhibitions_member/exhibitions_member.module.ts
+++ Back-end/src/exhibitions_member/exhibitions_member.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExhibitionsMemberService } from './exhibitions_member.service';
 import { ExhibitionsMemberController } from './exhibitions_member.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ExhibitionMember } from './entities/exhibition_member.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([ExhibitionMember])],
   controllers: [ExhibitionsMemberController],
   providers: [ExhibitionsMemberService],
 })
