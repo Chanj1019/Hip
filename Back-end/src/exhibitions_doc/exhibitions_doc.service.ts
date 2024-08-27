48f927db648e7b94080c4c40e99616779fabd111
cj <ckswls1019@naver.com>
2024-08-27 18:27:16 +0900

    수정:exhibitions_doc.service 코드 수정
    1.버켓 이름이 없을시 오류 반환
    2.s3에서 반환되는 url 환경 변수로 변경

--- Back-end/src/exhibitions_doc/exhibitions_doc.service.ts
+++ Back-end/src/exhibitions_doc/exhibitions_doc.service.ts
@@ -20,21 +20,25 @@ export class ExhibitionsDocService {
     @InjectRepository(Exhibition)
     private readonly exhibitionRepository: Repository<Exhibition>,
   )  {
-    // .env 파일에서 AWS 자격 증명 및 리전 가져오기
+   // .env 파일에서 AWS 자격 증명 및 리전 가져오기
     const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
     const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
     const AWS_REGION = process.env.AWS_REGION;
+    const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+
+    if (!S3_BUCKET_NAME) {
+        throw new InternalServerErrorException('S3 버킷 이름이 설정되지 않았습니다.');
+    }
 
     // S3 클라이언트 초기화
     this.s3 = new S3Client({
-      region: AWS_REGION,
-      credentials: {
-        accessKeyId: AWS_ACCESS_KEY_ID,
-        secretAccessKey: AWS_SECRET_ACCESS_KEY,
-      },
+        region: AWS_REGION,
+        credentials: {
+            accessKeyId: AWS_ACCESS_KEY_ID,
+            secretAccessKey: AWS_SECRET_ACCESS_KEY,
+        },
     });
   }
-
   async createExhibitionDoc(createExhibitionDocDto: CreateExhibitionsDocDto, file: Express.Multer.File): Promise<ExhibitionDoc> {
     const exhibitionId = Number(createExhibitionDocDto.exhibition_id);
     const exhibition = await this.exhibitionRepository.findOne({ where: { exhibition_id: exhibitionId } });
@@ -56,12 +60,12 @@ export class ExhibitionsDocService {
         });
         uploadResult = await this.s3.send(command); // S3에 파일 업로드
     } catch (error) {
-        console.log(error)
+        console.error(error); // logger로 변경 가능
         throw new InternalServerErrorException('파일 업로드에 실패했습니다.');
     }
 
     // S3에서 반환된 URL을 file_path에 저장
-    const filePath = `https://${process.env.AWS_REGION}.amazonaws.com/my-app-files-test/exhibitions/${uniqueFileName}`;
+    const filePath = `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/exhibitions/${uniqueFileName}`;
 
     // filePath가 비어있지 않은지 확인
     if (!filePath) {
@@ -76,6 +80,47 @@ export class ExhibitionsDocService {
 
     return await this.exhibitionsDocRepository.save(exhibitionDoc);
 }
+//   async createExhibitionDoc(createExhibitionDocDto: CreateExhibitionsDocDto, file: Express.Multer.File): Promise<ExhibitionDoc> {
+//     const exhibitionId = Number(createExhibitionDocDto.exhibition_id);
+//     const exhibition = await this.exhibitionRepository.findOne({ where: { exhibition_id: exhibitionId } });
+
+//     if (!exhibition) {
+//         throw new NotFoundException(`ID가 ${exhibitionId}인 전시를 찾을 수 없습니다.`);
+//     }
+
+//     // S3에 파일 업로드
+//     const uniqueFileName = `${uuidv4()}_${file.originalname}`;
+//     let uploadResult;
+
+//     try {
+//         const command = new PutObjectCommand({
+//             Bucket: process.env.S3_BUCKET_NAME,
+//             Key: `exhibitions/${uniqueFileName}`,
+//             Body: file.buffer,
+//             ContentType: file.mimetype,
+//         });
+//         uploadResult = await this.s3.send(command); // S3에 파일 업로드
+//     } catch (error) {
+//         console.log(error)
+//         throw new InternalServerErrorException('파일 업로드에 실패했습니다.');
+//     }
+
+//     // S3에서 반환된 URL을 file_path에 저장
+//     const filePath = `https://${process.env.AWS_REGION}.amazonaws.com/my-app-files-test/exhibitions/${uniqueFileName}`;
+
+//     // filePath가 비어있지 않은지 확인
+//     if (!filePath) {
+//         throw new InternalServerErrorException('파일 경로가 비어 있습니다.');
+//     }
+
+//     const exhibitionDoc = this.exhibitionsDocRepository.create({
+//         ...createExhibitionDocDto,
+//         exhibition,
+//         file_path: filePath,
+//     });
+
+//     return await this.exhibitionsDocRepository.save(exhibitionDoc);
+// }
 
   async findAll(): Promise<ExhibitionDoc[]> {
     return await this.exhibitionsDocRepository.find({
@@ -86,7 +131,7 @@ export class ExhibitionsDocService {
   async findOne(id: number): Promise<ExhibitionDoc> {
     const doc = await this.exhibitionsDocRepository.findOne({
       where: { exhibition_doc_id: id },
-      relations: ['exhibition'],
+      relations: ['exhibition,eme'],
     });
 
     if (!doc) {
@@ -94,7 +139,8 @@ export class ExhibitionsDocService {
     }
     return doc;
   }
-
+  
+  
   async update(id: number, updateExhibitionsDocDto: UpdateExhibitionsDocDto): Promise<ExhibitionDoc> {
     const doc = await this.findOne(id);
 
