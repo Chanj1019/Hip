8bb555be8c704ebffb3027e7c2c3a66c7856ea3d
jaeho kim <rlawogh1005@gmail.com>
2024-08-31 10:51:45 +0900

    추가: course_doc.module에 @nestjs/config, ConfigModule 추가

--- Back-end/src/course_doc/course_doc.module.ts
+++ Back-end/src/course_doc/course_doc.module.ts
@@ -4,9 +4,11 @@ import { CourseDocController } from './course_doc.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseDoc } from '../course_doc/entities/course_doc.entity'
 import {DocName} from '../doc_name/entities/doc_name.entity'
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([CourseDoc, DocName]),
     
   ],
