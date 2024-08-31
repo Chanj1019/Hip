1056c7844c6aad1187abe7e8450cf7601a0a0b7a
jaeho kim <rlawogh1005@gmail.com>
2024-08-31 10:53:12 +0900

    삭제: course_doc.entity에 doc_name_id, course_document_title, course_document_description 필드 삭제

--- Back-end/src/course_doc/entities/course_doc.entity.ts
+++ Back-end/src/course_doc/entities/course_doc.entity.ts
@@ -6,20 +6,11 @@ export class CourseDoc {
     @PrimaryGeneratedColumn()
     course_document_id: number;
 
-    @Column()
-    doc_name_id: number; // 주제 ID (Foreign Key)
-
-    @Column()
-    course_document_title: string;
-
     @CreateDateColumn()
     upload_date: Date; 
     
     @Column()
     file_path: string;
-
-    @Column()
-    course_document_description: string;
 
     @ManyToOne(() => DocName, (docname) => docname.courseDoc, { onDelete: 'CASCADE' })
     @JoinColumn({ name: 'doc_name_id' })
