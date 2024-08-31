277f335dede700fcaa857a942a5c4c7ac026ff51
cj <ckswls1019@naver.com>
2024-08-31 18:43:02 +0900

    수정:exhibition_intro 코드 수정
    1.엔드포인트에 반환 메세지 추가

--- Back-end/src/exhibition_intro/exhibition_intro.controller.ts
+++ Back-end/src/exhibition_intro/exhibition_intro.controller.ts
@@ -2,33 +2,45 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ExhibitionIntroService } from './exhibition_intro.service';
 import { CreateExhibitionIntroDto } from './dto/create-exhibition_intro.dto';
 import { UpdateExhibitionIntroDto } from './dto/update-exhibition_intro.dto';
+import { ExhibitionIntro } from './entities/exhibition_intro.entity';
 
 @Controller('exhibition-intro')
 export class ExhibitionIntroController {
   constructor(private readonly exhibitionIntroService: ExhibitionIntroService) {}
 
   @Post('register')
-  async create(@Body() createExhibitionIntroDto: CreateExhibitionIntroDto) {
-    return await this.exhibitionIntroService.create(createExhibitionIntroDto);
+  async create(@Body() createExhibitionIntroDto: CreateExhibitionIntroDto): Promise<{message: string; intro: ExhibitionIntro; }> {
+    const intro = await this.exhibitionIntroService.create(createExhibitionIntroDto);
+    return {message: 'intro 생성이 완료되었습니다.', intro};
   }
 
+
+ 
   @Get()
-  async findAll() {
-    return await this.exhibitionIntroService.findAll();
+  async findAll():Promise<{message:string; intro:ExhibitionIntro[]}> {
+    const intro = await this.exhibitionIntroService.findAll();
+    return {message:'전체 조회가 완료되었습니다',intro}
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.exhibitionIntroService.findOne(+id);
+  async findOne(@Param('id') id: string):Promise<{message:string; intro:ExhibitionIntro}> {
+    const intro = await this.exhibitionIntroService.findOne(+id);
+    return {message:'intro 조회를 완료했습니다',intro};
   }
 
+
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateExhibitionIntroDto: UpdateExhibitionIntroDto) {
-    return await this.exhibitionIntroService.update(+id, updateExhibitionIntroDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateExhibitionIntroDto: UpdateExhibitionIntroDto,
+  ): Promise<{ message: string; intro: ExhibitionIntro }> {
+    const intro = await this.exhibitionIntroService.update(+id, updateExhibitionIntroDto);
+    return { message: 'intro가 성공적으로 업데이트되었습니다.', intro };
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return await this.exhibitionIntroService.remove(+id);
+  async remove(@Param('id') id: string): Promise<{message:string}> {
+    await this.exhibitionIntroService.remove(+id);
+    return {message: 'intro가 삭제되었습니다'};
   }
 }
