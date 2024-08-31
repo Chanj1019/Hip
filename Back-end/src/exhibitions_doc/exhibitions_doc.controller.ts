90eeecd3b1b710880e4d754f756da3ab2dd1e333
cj <ckswls1019@naver.com>
2024-08-31 18:54:15 +0900

    수정:exhibitions_doc 코드 수정
    1.메세지 반환 추가

--- Back-end/src/exhibitions_doc/exhibitions_doc.controller.ts
+++ Back-end/src/exhibitions_doc/exhibitions_doc.controller.ts
@@ -11,36 +11,48 @@ export class ExhibitionsDocController {
   constructor(private readonly exhibitionDocsService: ExhibitionsDocService) {}
 
   @Get()
-  async findAll(): Promise<ExhibitionDoc[]> {
-    return await this.exhibitionDocsService.findAll();
+  async findAll(): Promise<{doc:ExhibitionDoc[];message:string}> {
+    const doc = await this.exhibitionDocsService.findAll();
+    return {message:'전체 자료 조회를 완료했습니다.',doc};
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: number): Promise<ExhibitionDoc> {
-    return await this.exhibitionDocsService.findOne(id);
+  async findOne(@Param('id') id: number): Promise<{doc:ExhibitionDoc; message:string}> {
+    const doc =await this.exhibitionDocsService.findOne(id);
+    return {message:'자료 조회를 완료했습니다.',doc};
   }
 
-  @Post('register')
+  // @Post('register')
+  // @UseInterceptors(FileInterceptor('file')) // 'file' 필드에서 파일을 업로드 받음
+  // async createExhibitionDoc(
+  //   @Body() createExhibitionDocDto: CreateExhibitionsDocDto,
+  //   @UploadedFile() file: Express.Multer.File, // 업로드된 파일을 가져옴
+  // ) {
+  //   // createExhibitionDoc 메소드를 호출하고 결과 반환
+  //   return this.exhibitionDocsService.createExhibitionDoc(createExhibitionDocDto,file);
+  // }
+    @Post('register')
   @UseInterceptors(FileInterceptor('file')) // 'file' 필드에서 파일을 업로드 받음
   async createExhibitionDoc(
     @Body() createExhibitionDocDto: CreateExhibitionsDocDto,
     @UploadedFile() file: Express.Multer.File, // 업로드된 파일을 가져옴
-  ) {
-    // createExhibitionDoc 메소드를 호출하고 결과 반환
-    return this.exhibitionDocsService.createExhibitionDoc(createExhibitionDocDto,file);
+  ): Promise<{ message: string; doc: any }> { // 반환 타입 정의
+    const doc = await this.exhibitionDocsService.createExhibitionDoc(createExhibitionDocDto, file);
+    return { message: '전시 문서가 성공적으로 등록되었습니다.', doc };
   }
-  
+
   @Put(':id')
   async update(
     @Param('id') id: number,
     @Body() updateExhibitionsDocDto: UpdateExhibitionsDocDto,
-  ): Promise<ExhibitionDoc> {
-    return await this.exhibitionDocsService.update(id, updateExhibitionsDocDto);
+  ): Promise<{doc:ExhibitionDoc; message:string}> {
+    const doc = await this.exhibitionDocsService.update(id, updateExhibitionsDocDto);
+    return {message: '전시 문서가 성공적으로 업데이트 되었습니다.',doc};
   } 
   
   @Delete(':id')
   async remove(@Param('id') id: number, @Res() res: Response): Promise<Response> {
       const message = await this.exhibitionDocsService.remove(id);
-      return res.status(200).json({ message });
+      return res.status(200).json({ message:'성공적으로 삭제되었습니다.' });
   }
 }
