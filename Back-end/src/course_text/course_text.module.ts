86d3a3cb69cabcdaac2b9bfef8d1420283766151
jaeho kim <rlawogh1005@gmail.com>
2024-09-01 13:32:35 +0900

    삭제: course_text 전체 삭제

--- Back-end/src/course_text/course_text.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CourseTextService } from './course_text.service';
-import { CourseTextController } from './course_text.controller';
-
-@Module({
-  controllers: [CourseTextController],
-  providers: [CourseTextService],
-})
-export class CourseTextModule {}
--- Back-end/src/course_text/dto/update-course_text.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCourseTextDto } from './create-course_text.dto';
-
-export class UpdateCourseTextDto extends PartialType(CreateCourseTextDto) {}
