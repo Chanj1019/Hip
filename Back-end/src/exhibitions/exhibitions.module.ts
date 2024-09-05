edc70c91f69408a8bd903afe8508beed3db65836
cj <ckswls1019@naver.com>
2024-09-05 23:40:06 +0900

    수정:exhibition.module 코드 수정
    1.imports에 usersmodule 추가

--- Back-end/src/exhibitions/exhibitions.module.ts
+++ Back-end/src/exhibitions/exhibitions.module.ts
@@ -3,12 +3,14 @@ import { ExhibitionService } from './exhibitions.service';
 import { ExhibitionController } from './exhibitions.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Exhibition } from './exhibition.entity';
+import { RolesGuard } from '../guards/roles.guard';
+import { UsersModule } from '../users/users.module';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Exhibition]),
-    
+    UsersModule,
   ],
-  providers: [ExhibitionService],
+  providers: [ExhibitionService,RolesGuard],
   controllers: [ExhibitionController]
 })
 export class ExhibitionModule {}
