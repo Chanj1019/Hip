c250970c326a05987f966129f8fc4b13f344119e
cj <ckswls1019@naver.com>
2024-09-06 22:01:46 +0900

    수정:exhibitions_doc module 코드 수정
    1.usersmodule 의존성 주입

--- Back-end/src/exhibitions_doc/exhibitions_doc.module.ts
+++ Back-end/src/exhibitions_doc/exhibitions_doc.module.ts
@@ -4,9 +4,13 @@ import { ExhibitionsDocController } from './exhibitions_doc.controller';
 import { ExhibitionsDocService } from './exhibitions_doc.service';
 import { ExhibitionDoc } from './entities/exhibition_doc.entity';
 import { Exhibition } from 'src/exhibitions/exhibition.entity';
+import { UsersModule } from '../users/users.module'; // UsersModule 임포트 추가
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ExhibitionDoc, Exhibition])],
+  imports: [
+    TypeOrmModule.forFeature([ExhibitionDoc, Exhibition]),
+    UsersModule, // UsersModule 추가
+  ],
   controllers: [ExhibitionsDocController],
   providers: [ExhibitionsDocService],
 })
