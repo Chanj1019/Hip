99d246db1edb55f167c2073bdfb59de625749704
cj <ckswls1019@naver.com>
2024-09-08 14:21:59 +0900

    수정:auth.module 코드 수정
    1.usermodule 임포트 , providers에 rolesguard 추가, global 적용

--- Back-end/src/auth/auth.module.ts
+++ Back-end/src/auth/auth.module.ts
@@ -1,21 +1,62 @@
-// auth.module.ts
-import { Module } from '@nestjs/common';
+// import { Module, Global } from '@nestjs/common'; // Global 임포트 추가
+// import { AuthService } from './auth.service';
+// import { AuthController } from './auth.controller';
+// import { JwtStrategy } from './jwt.strategy';
+// import { User } from '../users/user.entity'; 
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { JwtModule } from '@nestjs/jwt';
+// import { RolesGuard } from './roles.guard';
+// import { APP_GUARD } from '@nestjs/core';
+// import { UsersModule } from '../users/users.module';
+
+// @Global() // 전역 모듈로 설정
+// @Module({
+//     imports: [
+//         UsersModule,
+//         TypeOrmModule.forFeature([User]),
+//         JwtModule.register({
+//             secret: process.env.JWT_SECRET,
+//             signOptions: { expiresIn: '1h' },
+//         }),
+//     ],
+//     providers: [
+//         AuthService,
+//         JwtStrategy,
+//         {
+//             provide: APP_GUARD,
+//             useClass: RolesGuard, // RolesGuard를 전역 가드로 설정
+//         },
+//     ],
+//     controllers: [AuthController],
+//     exports: [AuthService], // 다른 모듈에서 AuthService를 사용할 수 있도록 export
+// })
+// export class AuthModule {}
+import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
-import { User } from '../users/user.entity'; // User 엔티티 임포트
+import { User } from '../users/user.entity'; 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
-
+import { RolesGuard } from './roles.guard';
+import { UsersModule } from '../users/users.module';
+@Global()
 @Module({
     imports: [
-        TypeOrmModule.forFeature([User]), // User 엔티티를 TypeORM 모듈에 등록
+        UsersModule,
+        TypeOrmModule.forFeature([User]),
         JwtModule.register({
-            secret: process.env.JWT_SECRET, // 비밀 키 설정
-            signOptions: { expiresIn: '1h' }, // 토큰 만료 시간 설정
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: '1h' },
         }),
     ],
-    providers: [AuthService, JwtStrategy],
+    providers: [
+        AuthService,
+        JwtStrategy,
+        RolesGuard
+        
+    ],
     controllers: [AuthController],
+    exports: [AuthService,RolesGuard],
 })
 export class AuthModule {}
