04823570b15d828c4b5922d79018256cd522435c
cj <ckswls1019@naver.com>
2024-09-08 14:22:50 +0900

    수정:exhibitions_member.module 코드 수정
    1.usermodule 임포트

--- Back-end/src/exhibitions_member/exhibitions_member.module.ts
+++ Back-end/src/exhibitions_member/exhibitions_member.module.ts
@@ -5,10 +5,12 @@ import { ExhibitionMember } from './entities/exhibition_member.entity';
 import { Exhibition } from 'src/exhibitions/exhibition.entity';
 import { ExhibitionService } from 'src/exhibitions/exhibitions.service';
 import { ExhibitionsMemberController } from './exhibitions_member.controller';
+import { UsersModule } from '../users/users.module'; // UsersModule 임포트 추가
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([ExhibitionMember, Exhibition]),
-    
+    UsersModule, // UsersModule 추가
   ],
   providers: [ExhibitionsMemberService,ExhibitionService],
   exports: [ExhibitionsMemberService],
