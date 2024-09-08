ba2aa53c6f3fd09477740ab8b760ec6783ea1392
jhk <qwertyjhk11@naver.com>
2024-09-08 16:30:38 +0900

    추가: 1. 필요한 모듈 import(TypeORM, etc) 2. @Module에 추가

--- Back-end/src/video_topic/video_topic.module.ts
+++ Back-end/src/video_topic/video_topic.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { VideoTopicService } from './video_topic.service';
 import { VideoTopicController } from './video_topic.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VideoTopic } from './entities/video_topic.entity'
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([VideoTopic]),
+    
+  ],
   controllers: [VideoTopicController],
   providers: [VideoTopicService],
 })
