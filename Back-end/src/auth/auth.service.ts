447b393a7662b8487d598d038653dae8150ddd30
cj <ckswls1019@naver.com>
2024-09-09 23:29:46 +0900

    수정:auth.service 코드 수정
    1.login 로직 변경

--- Back-end/src/auth/auth.service.ts
+++ Back-end/src/auth/auth.service.ts
@@ -14,24 +14,45 @@ export class AuthService {
         private readonly jwtService: JwtService,
     ) {}
 
-    async login(id: string, password: string): Promise<string> {
-        const user = await this.userRepository.findOne({ where: { id } });
-        console.log('User found:', user);
+    // async login(id: string, password: string): Promise<string> {
+    //     const user = await this.userRepository.findOne({ where: { id } });
+    //     console.log('User found:', user);
 
-        if (!user) {
-            throw new HttpException('User not found', HttpStatus.UNAUTHORIZED);
+    //     if (!user) {
+    //         throw new HttpException('User not found', HttpStatus.UNAUTHORIZED);
+    //     }
+
+    //     const isPasswordValid = await bcrypt.compare(password, user.password);
+    //     if (!isPasswordValid) {
+    //         throw new HttpException('Invalid password', HttpStatus.UNAUTHORIZED);
+    //     }
+
+    //     if (!process.env.JWT_SECRET) {
+    //         throw new Error('JWT_SECRET is not defined');
+    //     }
+
+    //     const token = this.jwtService.sign({ id: user.user_id });
+    //     return token;
+    // }
+    async login(id: string, password: string) {
+        
+        if (!process.env.JWT_SECRET) {
+            throw new Error('JWT_SECRET is not defined');
         }
-
+    
+        const user = await this.userRepository.findOne({ where: { id } });
+        
+        if (!user) {
+            throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+        }
+    
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
             throw new HttpException('Invalid password', HttpStatus.UNAUTHORIZED);
         }
-
-        if (!process.env.JWT_SECRET) {
-            throw new Error('JWT_SECRET is not defined');
-        }
-
+    
         const token = this.jwtService.sign({ id: user.user_id });
         return token;
     }
+    
 }
