9a6c71fba5d8991c82338b31360499399b9054a4
cj <ckswls1019@naver.com>
2024-09-10 22:39:40 +0900

    수정:courses.module 수정
    1.usermodule 의존성 주입

--- Back-end/src/courses/courses.module.ts
+++ Back-end/src/courses/courses.module.ts
@@ -5,11 +5,12 @@ import { Course } from './entities/course.entity'
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UCat } from '../ucat/entities/ucat.entity';
 import {DocName} from '../doc_name/entities/doc_name.entity'
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Course, UCat, DocName]),
-    
+    UsersModule,
   ],
   providers: [CoursesService],
   controllers: [CoursesController],
