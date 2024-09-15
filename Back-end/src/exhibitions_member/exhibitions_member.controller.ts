f6419a0019c1e045d5b15a8ad4a72e8388233e50
cj <ckswls1019@naver.com>
2024-09-16 04:19:00 +0900

    수정:exhibitions_member 관련 코드 수정
    1.authguard -> jwtauthguard

--- Back-end/src/exhibitions_member/exhibitions_member.controller.ts
+++ Back-end/src/exhibitions_member/exhibitions_member.controller.ts
@@ -6,13 +6,14 @@ import { UseInterceptors, UploadedFile } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { RolesGuard } from '../auth/roles.guard';
 import { Roles } from '../auth/roles.decorator';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard,RolesGuard)
 @Controller('exhibition-members')
 export class ExhibitionsMemberController {
     constructor(private readonly exhibitionsMemberService: ExhibitionsMemberService) {}
 
     @Post('register')
-    @UseGuards(RolesGuard)
     @Roles('admin')
     @UseInterceptors(FileInterceptor('file')) // 'file'은 업로드할 파일의 필드 이름
     async create(
@@ -36,7 +37,6 @@ export class ExhibitionsMemberController {
     }
 
     @Put(':id')
-    @UseGuards(RolesGuard)
     @Roles('admin')
     async update(@Param('id') id: number, @Body() updateData: Partial<CreateExhibitionsMemberDto>): Promise<{member:ExhibitionMember;message:string}> {
         const member = await this.exhibitionsMemberService.update(id, updateData);
@@ -44,20 +44,10 @@ export class ExhibitionsMemberController {
     }
 
     @Delete(':id')
-    @UseGuards(RolesGuard)
     @Roles('admin')
     async remove(@Param('id') id: number): Promise<{message:string}> {
         await this.exhibitionsMemberService.remove(id);
         return {message:'삭제 완료되었습니다.'}; // 삭제 완료 메시지 반환
     }
 
-    // @Get(':id/download')
-    // async download(@Param('id') id: number, @Res() res): Promise<void> {
-    // const fileStream = await this.exhibitionsMemberService.downloadFile(id);
-    // res.set({
-    //     'Content-Type': 'application/octet-stream',
-    //     'Content-Disposition': `attachment; filename=${id}.file`, // 적절한 파일 이름 설정
-    // });
-    // fileStream.pipe(res); // 스트림을 클라이언트로 파이핑
-    // }
 }
