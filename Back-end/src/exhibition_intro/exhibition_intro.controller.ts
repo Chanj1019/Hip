ac306786461c33cc98ab36694d5d35246dbb7f64
cj <ckswls1019@naver.com>
2024-09-15 21:38:36 +0900

    수정:exhibition_intro 관련 코드 수정
    1.controller에 가드 적용
    2.module에 의존성 주입

--- Back-end/src/exhibition_intro/exhibition_intro.controller.ts
+++ Back-end/src/exhibition_intro/exhibition_intro.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller , Get , Post , Body , Patch , Param , Delete , UseGuards } from '@nestjs/common';
 import { ExhibitionIntroService } from './exhibition_intro.service';
 import { CreateExhibitionIntroDto } from './dto/create-exhibition_intro.dto';
 import { UpdateExhibitionIntroDto } from './dto/update-exhibition_intro.dto';
 import { ExhibitionIntro } from './entities/exhibition_intro.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('exhibition-intro')
 export class ExhibitionIntroController {
   constructor(private readonly exhibitionIntroService: ExhibitionIntroService) {}
 
   @Post('register')
+  @UseGuards(JwtAuthGuard)
   async create(@Body() createExhibitionIntroDto: CreateExhibitionIntroDto): Promise<{message: string; intro: ExhibitionIntro; }> {
     const intro = await this.exhibitionIntroService.create(createExhibitionIntroDto);
     return {message: 'intro 생성이 완료되었습니다.', intro};
@@ -30,6 +32,7 @@ export class ExhibitionIntroController {
 
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   async update(
     @Param('id') id: string,
     @Body() updateExhibitionIntroDto: UpdateExhibitionIntroDto,
@@ -39,6 +42,7 @@ export class ExhibitionIntroController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   async remove(@Param('id') id: string): Promise<{message:string}> {
     await this.exhibitionIntroService.remove(+id);
     return {message: 'intro가 삭제되었습니다'};
