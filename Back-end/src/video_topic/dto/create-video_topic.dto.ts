24af10efab323f5f955e3f89414af81c97a6e38a
jhk <qwertyjhk11@naver.com>
2024-09-15 14:46:03 +0900

    추가: dto 정의, 유효성 검사

--- Back-end/src/video_topic/dto/create-video_topic.dto.ts
+++ Back-end/src/video_topic/dto/create-video_topic.dto.ts
@@ -1,1 +1,12 @@
-export class CreateVideoTopicDto {}
+import {IsString, MaxLength, IsOptional} from 'class-validator'
+
+export class CreateVideoTopicDto {
+    @IsString()
+    @MaxLength(255)
+    video_topic_title: string;
+
+    @IsString()
+    @IsOptional()
+    @MaxLength(255)
+    video_pa_topic_title?: string;
+}
