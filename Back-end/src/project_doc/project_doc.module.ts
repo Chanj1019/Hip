09b74154353560682c8fd627c6a445bd06d59518
cj <ckswls1019@naver.com>
2024-09-16 04:19:46 +0900

    수정:project_doc 관련 코드 수정
    1.module 의존성 추가
    2.controller에 권한제어 추가

--- Back-end/src/project_doc/project_doc.module.ts
+++ Back-end/src/project_doc/project_doc.module.ts
@@ -4,10 +4,12 @@ import { Project_doc } from './entities/project_doc.entity';
 import { ProjectDocService } from './project_doc.service';
 import { ProjectDocController } from './project_doc.controller';
 import { Project } from '../projects/entities/project.entity';
-import { Feedback } from 'src/feedback/entities/feedback.entity';
+import { Feedback } from '../feedback/entities/feedback.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Project_doc,Project,Feedback])],
+    imports: [TypeOrmModule.forFeature([Project_doc,Project,Feedback]),
+    UsersModule],
     providers: [ProjectDocService],
     controllers: [ProjectDocController],
     exports: [ProjectDocService],
