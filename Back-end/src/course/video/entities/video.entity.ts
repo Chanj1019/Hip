592983dee791d7d64eaf4987219e5cf6df21d62e
jhk <qwertyjhk11@naver.com>
2024-09-18 21:47:45 +0900

    추가: Entity 정의 1. 필요한 모듈 추가 2. ManyToOne 정의

--- Back-end/src/course/video/entities/video.entity.ts
+++ Back-end/src/course/video/entities/video.entity.ts
@@ -1,1 +1,15 @@
-export class Video {}
+import { VideoTopic } from 'src/course/video_topic/entities/video_topic.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+
+@Entity()
+export class Video {
+    @PrimaryGeneratedColumn()
+    video_id: number;
+
+    @Column()
+    video_url: string;
+
+    @ManyToOne(() => VideoTopic, (videotopic) => videotopic.videos, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'video_topic_id' })
+    videoTopic: VideoTopic;
+}
