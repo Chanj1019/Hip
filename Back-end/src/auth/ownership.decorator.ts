836f54abe3816a278c95adf12fcd078a0993593f
cj <ckswls1019@naver.com>
2024-09-22 14:11:21 +0900

    삭제:ownership.decorator 파일 삭제
    1.ownership.decorator 파일 삭제

--- Back-end/src/auth/ownership.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const Ownership = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user.user_id; // 현재 사용자 ID 반환
-  },
-);
