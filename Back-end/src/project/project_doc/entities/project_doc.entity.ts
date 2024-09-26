abf05079c0dcae83a3f7982866635f37aaebae6d
jungmin <1sonjungmin@naver.com>
2024-09-27 00:53:58 +0900

    fix: 필드 수정 - 이름 변경 및 삭제 - 관계 열  추가 - 속성 추가

--- Back-end/src/project/project_doc/entities/project_doc.entity.ts
+++ Back-end/src/project/project_doc/entities/project_doc.entity.ts
@@ -1,23 +1,27 @@
 import { Feedback } from '../../feedback/entities/feedback.entity';
 import { Project } from '../../projects/entities/project.entity';
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany, JoinColumn } from 'typeorm';
 
 @Entity()
-export class Project_doc {
+export class ProjectDoc {
     @PrimaryGeneratedColumn()
     project_doc_id: number;
 
     @Column({ type: 'varchar', length: 255 })
     description: string;
 
-    @Column()
+    @Column({ type: 'varchar', length: 255 })
     file_path: string; // 파일 경로
 
     // project_doc - project
     @ManyToOne(() => Project, (project) => project.project_docs)
+    @JoinColumn({ name: 'projectId' }) // 외래 키의 이름을 명시
     project: Project;
+
+    @Column({ type: 'int', nullable: false })
+    projectId: number;
 
     // project_Doc - feedback
     @OneToMany(() => Feedback, (feedback) => feedback.projectDoc)
-    feedbacks: Feedback;
+    feedbacks: Feedback[];
 }
