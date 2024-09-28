a3f0c334ec03279e5a0515990d3c52094cd74be2
jungmin <1sonjungmin@naver.com>
2024-09-28 17:04:33 +0900

    feat: course registration controller - CRUD 구현 -엔드포인트 작성 - 반환 메시지 작성

--- Back-end/src/course/course_registration/course_registration.controller.ts
+++ Back-end/src/course/course_registration/course_registration.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { CourseRegistrationService } from './course_registration.service';
 import { CreateCourseRegistrationDto } from './dto/create-course_registration.dto';
 import { UpdateCourseRegistrationDto } from './dto/update-course_registration.dto';
@@ -8,35 +8,59 @@ import { Roles } from '../../auth/roles.decorator';
 
 @Controller('courses/:course/courseRegistration')
 export class CourseRegistrationController {
-  constructor(private readonly courseRegistrationService: CourseRegistrationService) {}
+    constructor(private readonly courseRegistrationService: CourseRegistrationService) {}
 
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Post('register')
-  @Roles('instructor','student')
-  create(@Body() createCourseRegistrationDto: CreateCourseRegistrationDto) {
-    return this.courseRegistrationService.create(createCourseRegistrationDto);
-  }
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Post('register')
+    @Roles('instructor', 'student')
+    async create(@Body() createCourseRegistrationDto: CreateCourseRegistrationDto, @Request() req, @Param('course') course_id: number) {
+        // 로그인된 user_id 저장
+        const user_id = req.user.user_id;
+        createCourseRegistrationDto.userId = user_id;
 
-  @Get()
-  @Roles('admin')
-  findAll() {
-    return this.courseRegistrationService.findAll();
-  }
+        // 어떤 강의 인지 저장
+        createCourseRegistrationDto.courseId = course_id;
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.courseRegistrationService.findOne(+id);
-  // }
+        const data = await this.courseRegistrationService.create(createCourseRegistrationDto);
 
-  @Patch(':id')
-  @Roles('admin')
-  update(@Param('id') id: string, @Body() updateCourseRegistrationDto: UpdateCourseRegistrationDto) {
-    return this.courseRegistrationService.update(+id, updateCourseRegistrationDto);
-  }
+        return { 
+            message: "수강 신청이 완료되었습니다.",
+            data: data,
+        };
+    }
 
-  @Delete(':id')
-  @Roles('instructor','student')
-  remove(@Param('id') id: string) {
-    return this.courseRegistrationService.remove(+id);
-  }
+    @Get()
+    @Roles('admin')
+    async findAll() {
+        const data = this.courseRegistrationService.findAll();
+        return {
+            message: "수강 신청 정보가 조회되었습니다.",
+            data: data,
+        }
+    }
+
+    // @Get(':id')
+    // findOne(@Param('id') id: string) {
+    //     return this.courseRegistrationService.findOne(+id);
+    // }
+
+    @Patch(':id')
+    @Roles('admin')
+    async update(@Param('id') id: number, @Body() updateCourseRegistrationDto: UpdateCourseRegistrationDto) {
+        const data = this.courseRegistrationService.update(id, updateCourseRegistrationDto);
+        return {
+            message: "수강 신청이 정보가 업데이트되었습니다.",
+            data: data,
+        }
+    }
+
+    @Delete(':id')
+    @Roles('instructor', 'student')
+    remove(@Param('id') id: number) {
+        const data = this.courseRegistrationService.remove(id);
+        return {
+            message: "수강 신청이 취소되었습니다.",
+            data: data,
+        }
+    }
 }
