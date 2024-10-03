095a32d8dcf727e2cc3a1198c7c78c13cb3ca692
cj <ckswls1019@naver.com>
2024-10-03 17:56:52 +0900

    수정:courses.module 수정
    1.가드 사용을 위한 의존성 주입

--- Back-end/src/course/courses/courses.module.ts
+++ Back-end/src/course/courses/courses.module.ts
@@ -8,11 +8,14 @@ import { UsersModule } from '../../user/users.module';
 import { VideoTopic } from 'src/course/video_topic/entities/video_topic.entity';
 import { ProjectsModule } from '../../project/projects/projects.module';
 import { ExhibitionModule } from '../../exhibition/exhibitions/exhibitions.module';
+import { ExhibitionsDocModule } from '../../exhibition/exhibitions_doc/exhibitions_doc.module';
+import { CourseDocModule } from '../course_doc/course_doc.module';
+import { DocNameModule } from '../doc_name/doc_name.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Course, DocName, VideoTopic]),
-        UsersModule,ProjectsModule,ExhibitionModule
+        UsersModule, ProjectsModule, ExhibitionModule, ExhibitionsDocModule, CourseDocModule, DocNameModule
     ],
     providers: [CoursesService],
     controllers: [CoursesController],
