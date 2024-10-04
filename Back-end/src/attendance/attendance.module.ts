56ba1b0bb5d5acee49d029d31383d4f08650e0d5
cj <ckswls1019@naver.com>
2024-10-04 16:59:04 +0900

    수정:attendance.module 수정
    1.user,course 추가
    2.typeorm 추가

--- Back-end/src/attendance/attendance.module.ts
+++ Back-end/src/attendance/attendance.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AttendanceService } from './attendance.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AttendanceController } from './attendance.controller';
+import { AttendanceService } from './attendance.service';
+import { Attendance } from './entities/attendance.entity';
+import { User } from '../user//user.entity'; // User 엔티티 임포트
+import { Course } from '../course/courses/entities/course.entity'; // Course 엔티티 임포트
 
 @Module({
-  controllers: [AttendanceController],
-  providers: [AttendanceService],
+    imports: [
+        TypeOrmModule.forFeature([Attendance, User, Course]), // Attendance, User, Course 엔티티를 TypeORM 모듈에 등록
+    ],
+    controllers: [AttendanceController],
+    providers: [AttendanceService],
+    exports: [AttendanceService], // 다른 모듈에서 서비스 사용 가능하도록 내보내기
 })
 export class AttendanceModule {}
