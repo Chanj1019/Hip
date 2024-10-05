c55bc971261b0a1e8c870327eec4877e0d494369
jungmin <1sonjungmin@naver.com>
2024-10-05 16:37:27 +0900

    fix: 엔티티 필요 없는 필드 삭제 및 수정 필드 변경에 따라 로직 수정

--- Back-end/src/project/project_registration/registration.service.ts
+++ Back-end/src/project/project_registration/registration.service.ts
@@ -19,57 +19,52 @@ export class ProjectRegistrationService {
         private readonly userRepository: Repository<User>,
     ) {}
 
-    async create(createProjectRegistrationDto: CreateProjectRegistrationDto): Promise<ProjectRegistration> {
-        // 이미 해당 프로젝트에 참가 신청이 되어 있을 때
-        const projectId = createProjectRegistrationDto.projectId;
-        const existingProject = await this.projectsRepository.findOne({ where: { project_id: projectId } });
-        const userId = createProjectRegistrationDto.userId;
-        const existingUser = await this.userRepository.findOne({ where: { user_id: userId } });
-    
-        const existingProjectRegistration = await this.projectRegistrationRepository.findOne({
+    // 이미 수강신청이 되어 있는지 확인하는 함수
+    async isEnrolled(projectId: number, userId: number): Promise<boolean> {
+        const existingEnrollment = await this.projectRegistrationRepository.findOne({
             where: {
-                user: existingUser,
-                project: existingProject,
+                project: { project_id: projectId }, // 프로젝트 ID로 필터링
+                user: { user_id: userId }, // 사용자 ID로 필터링
             },
         });
-    
-        if (existingProjectRegistration) {
+
+        return !!existingEnrollment; // 이미 존재하면 true, 없으면 false
+    }
+
+
+    async create(createProjectRegistrationDto: CreateProjectRegistrationDto, projectId: number, loginedUser: number): Promise<ProjectRegistration> {
+        // 이미 해당 프로젝트에 참가 신청이 되어 있을 때
+        const isAlreadyEnrolled = await this.isEnrolled(projectId, loginedUser);
+        if(isAlreadyEnrolled) {
             throw new ConflictException('이미 신청된 프로젝트입니다.');
         }
     
         // 처음 참가 신청
         const projectRegistration = this.projectRegistrationRepository.create(createProjectRegistrationDto);
+        projectRegistration.user = await this.userRepository.findOneBy({ user_id: loginedUser });  // 특정 사용자와 연결된 정보
+        projectRegistration.project = await this.projectsRepository.findOneBy({ project_id: projectId });  // 특정 프로젝트와 연결된 정보
         return await this.projectRegistrationRepository.save(projectRegistration);
-    }    
+    }
     
     // user 참고하여 user_name과 id를 함께 반환
-    async findAll(): Promise<{ registration: ProjectRegistration; userName: string; userId: number }[]> {
-        const registrations = await this.projectRegistrationRepository
-            .createQueryBuilder('registration')
-            .leftJoinAndSelect('registration.user', 'user')
-            .select([   
-                'registration',
-                'user.user_name',
-                'user.id',
-            ])
-            .getMany();
-    
-        return registrations.map(registration => ({
-            registration,
+    async findAll(): Promise<{ projectRegistration: ProjectRegistration; userName: string; userId: string, projectTopic: string }[]> {
+        const info = await this.projectRegistrationRepository.find({ relations: ['user', 'project'] });  // 각 등록에 대한 사용자와 프로젝트 정보
+        return info.map(registration => ({
+            projectRegistration: registration,
             userName: registration.user.user_name,
-            userId: registration.userId,
+            userId: registration.user.id,
+            projectTopic: registration.project.topic,
         }));
-    }    
-    
+    }
 
     async findOne(id: number): Promise<ProjectRegistration> {
-        const registration = await this.projectRegistrationRepository.findOne({ where: { registration_id: id } });
+        const registration = await this.projectRegistrationRepository.findOne({ where: { project_registration_id: id } });
         this.handleNotFound(registration, id);
         return registration;
     }
 
     async update(id: number, updateProjectRegistrationDto: UpdateProjectRegistrationDto): Promise<ProjectRegistration> {
-        const registration = await this.projectRegistrationRepository.findOne({ where: { registration_id: id } });
+        const registration = await this.projectRegistrationRepository.findOne({ where: { project_registration_id: id } });
         this.handleNotFound(registration, id);
 
         Object.assign(registration, updateProjectRegistrationDto);
@@ -77,7 +72,7 @@ export class ProjectRegistrationService {
     }    
 
     async remove(id: number): Promise<void> {
-        const registration = await this.projectRegistrationRepository.findOne({ where: { registration_id: id } });
+        const registration = await this.projectRegistrationRepository.findOne({ where: { project_registration_id: id } });
         this.handleNotFound(registration, id);
         await this.projectRegistrationRepository.delete(id);
     }
