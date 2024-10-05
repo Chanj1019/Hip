a739436a553c00135d272859f1a9e240e2ecefb7
cj <ckswls1019@naver.com>
2024-10-05 16:39:27 +0900

    수정:create-attendance.dto 수정
    1.field 삭제

--- Back-end/src/attendance/dto/create-attendance.dto.ts
+++ Back-end/src/attendance/dto/create-attendance.dto.ts
@@ -3,12 +3,4 @@ import { IsEnum, IsNumber } from 'class-validator';
 export class CreateAttendanceDto {
     @IsNumber()
     courseId: number;
-
-    @IsEnum(['present', 'absent', 'late'])
-    field: 'present' | 'absent' | 'late';
-
-    constructor(partial: Partial<CreateAttendanceDto>) {
-        Object.assign(this, partial);
-        this.field = this.field || 'absent'; // field의 기본값을 'absent'로 설정
-    }
 }
