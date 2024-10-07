656f8cf54fccb53c6fb1b66a7f4fe1fb128f517c
jhk <qwertyjhk11@naver.com>
2024-10-07 18:57:28 +0900

    수정: attendance module에 courseregistration 추가

--- Back-end/src/attendance/attendance.module.ts
+++ Back-end/src/attendance/attendance.module.ts
@@ -5,10 +5,11 @@ import { AttendanceService } from './attendance.service';
 import { Attendance } from './entities/attendance.entity';
 import { User } from '../user//user.entity'; // User 엔티티 임포트
 import { Course } from '../course/courses/entities/course.entity'; // Course 엔티티 임포트
+import { CourseRegistration } from 'src/course/course_registration/entities/course_registration.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Attendance, User, Course]), // Attendance, User, Course 엔티티를 TypeORM 모듈에 등록
+        TypeOrmModule.forFeature([Attendance, User, Course, CourseRegistration]), // Attendance, User, Course 엔티티를 TypeORM 모듈에 등록
     ],
     controllers: [AttendanceController],
     providers: [AttendanceService],
