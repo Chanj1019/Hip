75e9c8167450fd45d35f33f6c00972c55ce90854
cj <ckswls1019@naver.com>
2024-10-09 16:06:49 +0900

    수정:project_doc.module 수정
    1.users.module 과 project_doc.module 순환 의존성 추가

--- Back-end/src/user/users.module.ts
+++ Back-end/src/user/users.module.ts
@@ -10,6 +10,7 @@ import { ExhibitionModule } from 'src/exhibition/exhibitions/exhibitions.module'
 import { ExhibitionsDocModule } from 'src/exhibition/exhibitions_doc/exhibitions_doc.module';
 import { CourseDocModule } from 'src/course/course_doc/course_doc.module';
 import { DocNameModule } from 'src/course/doc_name/doc_name.module';
+import { ProjectDocModule } from 'src/project/project_doc/project_doc.module';
 dotenv.config();
 
 // ProjectsModule 임포트
@@ -22,6 +23,7 @@ dotenv.config();
         forwardRef(() => ExhibitionsDocModule),
         forwardRef(() => CourseDocModule),
         forwardRef(() => DocNameModule),
+        forwardRef(() => ProjectDocModule),
          // ProjectsModule을 forwardRef로 임포트
     ],
     providers: [UsersService,HashService],
