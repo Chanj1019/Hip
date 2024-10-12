b665e7ae232a02fe153e208eae7ccea9d1d3b720
jungmin <1sonjungmin@naver.com>
2024-10-12 17:02:40 +0900

    fix: 수강신청 조회 response dto 생성

---
+++ Back-end/src/course/course_registration/dto/get-private-courseregistration.dto.ts
@@ -0,0 +1,16 @@
+// import { CourseResponseDto } from '../../../course/courses/dto/course-response.dto';
+// import { Registration } from '../../../enums/role.enum';
+// import { CourseRegistration } from '../entities/course_registration.entity';
+
+// export class GetPrivateResponseCourseRegistrationDto {
+
+//     course_registration_status: Registration;  // 수강 신청 상태
+
+//     currentCourse: CourseResponseDto; 
+
+//     //  생성자: CourseRegistration 객체에서 초기화
+//     constructor(courseRegistration: CourseRegistration) {
+//         this.course_registration_status = courseRegistration.course_registration_status;
+//         this.currentCourse = new CourseResponseDto(courseRegistration.course); // 강의 정보
+//     }
+// }
