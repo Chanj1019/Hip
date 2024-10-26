54082676756372a77c861ef8b4eaaefa84bae75f
jhk <qwertyjhk11@naver.com>
2024-10-26 10:15:45 +0900

    추가: course_registration권한에 admin 추가

--- Back-end/src/course/course_registration/course_registration.controller.ts
+++ Back-end/src/course/course_registration/course_registration.controller.ts
@@ -12,7 +12,7 @@ export class CourseRegistrationController {
 
     @UseGuards(JwtAuthGuard, RolesGuard)
     @Post('register')
-    @Roles('instructor', 'student')
+    @Roles('instructor', 'student', 'admin')
   
     async create(
         @Body() createCourseRegistrationDto: CreateCourseRegistrationDto, 
