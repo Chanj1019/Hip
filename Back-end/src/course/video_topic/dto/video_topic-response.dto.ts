ecb42fa91a8c7275d055262d4bb802458cb61689
jhk <qwertyjhk11@naver.com>
2024-11-04 20:36:25 +0900

    추가: VideoTopicResponseDto에 file_path 추가

--- Back-end/src/course/video_topic/dto/video_topic-response.dto.ts
+++ Back-end/src/course/video_topic/dto/video_topic-response.dto.ts
@@ -4,10 +4,12 @@ export class VideoTopicResponseDto {
     video_topic_id: number;
     video_topic_title: string;
     video_pa_topic_id: number;
+    file_path: string
 
     constructor(video: VideoTopic) {
         this.video_topic_id = video.video_topic_id;
         this.video_topic_title = video.video_topic_title;
         this.video_pa_topic_id = video.video_pa_topic_id;
+        this.file_path = video.file_path;
     }
 }
