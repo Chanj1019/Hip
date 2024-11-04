09a7474fa77198d9cf4d4711d24835c4393d5346
jhk <qwertyjhk11@naver.com>
2024-11-04 20:36:03 +0900

    추가: DocNameResponseDto 작성

---
+++ Back-end/src/course/doc_name/dto/doc_name-with-coursedoc-response.dto.ts
@@ -0,0 +1,16 @@
+import { CourseDocResponseDto } from "src/course/course_doc/dto/course_doc-response.dto";
+import { DocName } from "../entities/doc_name.entity";
+
+export class DocNameResponseDto {
+    topic_id: number;
+    topic_title: string;
+    pa_topic_id: number;
+    course_doc: CourseDocResponseDto[];
+    
+    constructor(docName: DocName) {
+        this.topic_id = docName.topic_id;
+        this.topic_title = docName.topic_title;
+        this.pa_topic_id = docName.pa_topic_id;
+        this.course_doc = docName.courseDocs ? docName.courseDocs.map(CourseDoc => new CourseDocResponseDto(CourseDoc)): [];
+    }
+}
