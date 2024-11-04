b3776557e731ac56135183895e43d48ceb9b6149
jhk <qwertyjhk11@naver.com>
2024-11-04 20:32:11 +0900

    수정: 클래스명 변경 삭제: course_doc id

--- Back-end/src/course/course_doc/dto/course_doc-response.dto.ts
+++ Back-end/src/course/course_doc/dto/course_doc-response.dto.ts
@@ -1,12 +1,10 @@
 import { CourseDoc } from "../entities/course_doc.entity";
 
-export class DocNameResponseDto {
-    course_document_id: number;
+export class CourseDocResponseDto {
     upload_date: Date;
     file_path: string;
 
     constructor(courseDoc: CourseDoc) {
-        this.course_document_id = courseDoc.course_document_id;
         this.upload_date = courseDoc.upload_date;
         this.file_path = courseDoc.file_path;
     }
