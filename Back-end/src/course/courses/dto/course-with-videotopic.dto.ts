5f94d4ff866f522435ba718d512cbe048e060499
jhk <qwertyjhk11@naver.com>
2024-11-04 20:34:13 +0900

    추가: CourseWithVideoTopicResponseDto 작성

---
+++ Back-end/src/course/courses/dto/course-with-videotopic.dto.ts
@@ -0,0 +1,22 @@
+import { Course } from '../entities/course.entity';
+import { DocNameResponseDto } from 'src/course/doc_name/dto/doc_name-with-coursedoc-response.dto';
+
+export class CourseWithVideoTopicResponseDto {
+    course_title: string;
+    description: string;
+    instructor_name: string;
+    course_notice: string;
+    generation: string;
+    docName: DocNameResponseDto[];
+
+    constructor(course: Course) {
+        this.course_title = course.course_title;
+        this.description = course.description;
+        this.instructor_name = course.instructor_name;
+        this.course_notice = course.course_notice;
+        this.generation = course.generation;
+        this.docName = course.docName 
+            ? course.docName.map(docName => new DocNameResponseDto(docName)) 
+            : [];
+    }
+}
