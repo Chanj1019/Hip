205bf2fb190f99583ecb537b35e6e5938d01f935
jhk <qwertyjhk11@naver.com>
2024-11-05 11:34:11 +0900

    삭제: ResponseDto 새로 작성

--- Back-end/src/user/dto/user-courses-projects.response.dto/course.dto.ts
+++
@@ -1,8 +0,0 @@
-// src/user/dto/course.dto.ts
-export class CourseDto {
-    course_id: number;
-    course_title: string;
-    description: string;
-    instructor_name: string;
-    course_notice?: string;  // nullable 속성은 선택적
-}
