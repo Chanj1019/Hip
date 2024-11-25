1dbbca1c81e226a59af4dd72077ff38b217941c8
jhk <qwertyjhk11@naver.com>
2024-11-25 17:01:42 +0900

    추가: 파라미터 url: string 추가

--- Back-end/src/course/video/dto/video-response.dto.ts
+++ Back-end/src/course/video/dto/video-response.dto.ts
@@ -3,9 +3,11 @@ import { Video } from "../entities/video.entity";
 export class VideoResponseDto {
     video_id: number;
     video_title: string;
+    url: string;
 
     constructor(video: Video) {
         this.video_id = video.video_id;
         this.video_title = video.video_title;
+        this.url = video.video_url
     }
 }
