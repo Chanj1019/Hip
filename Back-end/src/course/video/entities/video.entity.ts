5db35b722b47aa8724978609a9a5a2e2b87d5f1b
qwertyjhk11 <qwertyjhk11@naver.com>
2024-11-28 15:04:55 +0900

    추가: 제약조건 추가

--- Back-end/src/course/video/entities/video.entity.ts
+++ Back-end/src/course/video/entities/video.entity.ts
@@ -17,7 +17,7 @@ export class Video {
     @JoinColumn({ name: 'video_topic_id' })
     videoTopic: VideoTopic;
 
-    @Column({ nullable: true }) // nullable을 true로 설정
+    @Column({ nullable: true, type: 'text', name: 'summary' }) // nullable을 true로 설정
     @IsOptional()
-    Summary?: string; // 선택적 속성
+    summary?: string; // 선택적 속성
 }
