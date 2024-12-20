4efa494f253f14dbf2eeb2ecd81095031d4fcc01
jhk <qwertyjhk11@naver.com>
2024-12-20 17:27:34 +0900

    수정: ProjectDocController에서 ApiResponse와 ProjectDocResponseDto 사용 및 역할 기반 접근 제어 주석 처리

--- Back-end/src/project/project_doc/project_doc.controller.ts
+++ Back-end/src/project/project_doc/project_doc.controller.ts
@@ -9,52 +9,59 @@ import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 import { RolesGuard } from '../../auth/roles.guard';
 import { Roles } from '../../auth/roles.decorator';
 import { ApprovedStudentGuard } from '../../auth/project.approved.guard';
+import { ProjectDocResponseDto } from './dto/project_doc-response.dto';
+import { ApiResponse } from 'src/common/api-response.dto';
 @UseGuards(JwtAuthGuard, RolesGuard, ApprovedStudentGuard)
 @Controller('projects/:projectId/projectDocs')
 export class ProjectDocController {
     constructor(private readonly projectDocsService: ProjectDocService) {}
 
     @Post('register')
-    @Roles('instructor','student','admin')
+    // @Roles('instructor','student','admin')
     @UseInterceptors(FileInterceptor('file'))  // 'file' 필드에서 파일을 업로드 받음
     async create(
         @Param('projectId') projectId: number,
         @Body() createProjectDocDto: CreateProjectDocDto, 
         @UploadedFile() file: Express.Multer.File,// 업로드된 파일을 가져옴
-    ): Promise<{ message: string; doc: any }> {
-
-        const doc = await this.projectDocsService.create(projectId, createProjectDocDto, file);
-        return { message: '프로젝트 문서가 성공적으로 등록되었습니다.', doc };
+    ): Promise<ApiResponse<ProjectDocResponseDto>> {
+        const data = await this.projectDocsService.create(projectId, createProjectDocDto, file);
+        const responseData = new ProjectDocResponseDto(data);
+        return new ApiResponse<ProjectDocResponseDto>(200, '프로젝트 문서가 성공적으로 등록되었습니다.', responseData);
     }
 
     @Get()
-    async findAll(@Param('projectId') projectId: number): Promise<{ doc: ProjectDoc[]; message: string }> {
-        const doc = await this.projectDocsService.findAll(projectId);
-        return { message: '전체 프로젝트 자료 조회를 완료했습니다.', doc };
+    async findAll(
+        @Param('projectId') projectId: number
+    ): Promise<ApiResponse<ProjectDocResponseDto[]>> {
+        const data = await this.projectDocsService.findAll(projectId);
+        const responseData = data.map(doc => new ProjectDocResponseDto(doc));
+        return new ApiResponse<ProjectDocResponseDto[]>(200, '전체 프로젝트 자료 조회를 완료했습니다.', responseData );
     }
 
     @Get(':id/read')
     async findOne(
         @Param('id') id: number, 
         @Param('projectId') projectId: number
-    ): Promise<{ doc: ProjectDoc; message: string }> {
-        const doc = await this.projectDocsService.findOne(id, projectId);
-        return { message: '프로젝트 자료 조회를 완료했습니다.', doc };
+    ): Promise<ApiResponse<ProjectDocResponseDto>> {
+        const data = await this.projectDocsService.findOne(id, projectId);
+        const responseData = new ProjectDocResponseDto(data);
+        return new ApiResponse<ProjectDocResponseDto>(200, '프로젝트 문서 조회를 완료했습니다.', responseData); 
     }
     
     @Put(':id/update')
-    @Roles('instructor','student','admin')
+    // @Roles('instructor','student','admin')
     async update(
         @Param('id') id: number, 
         @Body() updateProjectDocDto: UpdateProjectDocDto,
         @Param('projectId') projectId: number
-    ): Promise<{ doc: ProjectDoc; message: string }> {
-        const doc = await this.projectDocsService.update(id, updateProjectDocDto, projectId);
-        return { message: '프로젝트 문서가 성공적으로 업데이트 되었습니다.', doc };
+    ): Promise<ApiResponse<ProjectDocResponseDto>> {
+        const data = await this.projectDocsService.update(id, updateProjectDocDto, projectId);
+        const responseData = new ProjectDocResponseDto(data);
+        return new ApiResponse<ProjectDocResponseDto>(200, '프로젝트 문서가 성공적으로 수정되었습니다.', responseData);
     }
 
     @Delete(':id/delete')
-    @Roles('instructor','student','admin')
+    // @Roles('instructor','student','admin')
     async remove(
         @Param('id') id: number,
         @Param('projectId') projectId: number
