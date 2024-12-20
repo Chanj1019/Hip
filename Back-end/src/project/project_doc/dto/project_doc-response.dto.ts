f8283a34499fa9eada67f9807acfe633c84982c5
jhk <qwertyjhk11@naver.com>
2024-12-20 17:27:41 +0900

    추가: ProjectDocResponseDto 클래스 작성

---
+++ Back-end/src/project/project_doc/dto/project_doc-response.dto.ts
@@ -0,0 +1,19 @@
+import { ProjectDoc } from '../entities/project_doc.entity';
+import { FeedbackResponseDto } from '../../feedback/dto/feedback-response.dto';
+import { ProjectResponseDto } from 'src/project/projects/dto/project-response.dto';
+
+export class ProjectDocResponseDto {
+    project_doc_id: number;
+    description: string;
+    file_path: string;
+    project_data: ProjectResponseDto;
+    feedback_data: FeedbackResponseDto[];
+
+    constructor(projectDoc: ProjectDoc) {
+        this.project_doc_id = projectDoc.project_doc_id;
+        this.description = projectDoc.description;
+        this.file_path = projectDoc.file_path;
+        this.project_data = new ProjectResponseDto(projectDoc.project);
+        this.feedback_data = projectDoc.feedbacks.map(feedback => new FeedbackResponseDto(feedback));
+    }
+}
