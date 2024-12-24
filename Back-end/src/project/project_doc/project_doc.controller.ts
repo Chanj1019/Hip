524fdec952286308a25f61a6f1243e6a5af4ba8d
jhk <qwertyjhk11@naver.com>
2024-12-24 11:24:37 +0900

    수정: 반환 타입 수정 및 컨벤션 정리리

--- Back-end/src/project/project_doc/project_doc.controller.ts
+++ Back-end/src/project/project_doc/project_doc.controller.ts
@@ -3,39 +3,55 @@ import { ProjectDocService } from './project_doc.service';
 import { CreateProjectDocDto } from './dto/create-project_doc.dto';
 import { UpdateProjectDocDto } from './dto/update-project_doc.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ProjectDocResponseDto } from './dto/project_doc-response.dto';
+import { ApiResponse } from 'src/common/api-response.dto';
 
 @Controller('projects/:projectId/:projectDocTitleId/project-doc')
 export class ProjectDocController {
   constructor(private readonly projectDocService: ProjectDocService) {}
 
   @Post()
-  @UseInterceptors(FileInterceptor('file'))  // 'file' 필드에서 파일을 업로드 받음
-  create(
+  @UseInterceptors(FileInterceptor('file'))
+  async create(
     @Param('projectId') projectId: number,
     @Param('projectDocTitleId') projectDocTitleId: number,
     @Body() createProjectDocDto: CreateProjectDocDto,
     @UploadedFile() file: Express.Multer.File,// 업로드된 파일을 가져옴
-  ) {// 업로드된 파일을 가져옴
-    return this.projectDocService.create(projectId, projectDocTitleId, createProjectDocDto, file);
+  ): Promise<ApiResponse<ProjectDocResponseDto>> {// 업로드된 파일을 가져옴
+    const data = await this.projectDocService.create(projectId, projectDocTitleId, createProjectDocDto, file);
+    return new ApiResponse<ProjectDocResponseDto>(200, "생성", data);
   }
 
   @Get()
-  findAll() {
-    return this.projectDocService.findAll();
+  findAll(
+
+  ): Promise<ApiResponse<ProjectDocResponseDto[]>> {
+    const data = this.projectDocService.findAll();
+    return new ApiResponse<ProjectDocResponseDto[]>(200, "전체 조회", data);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.projectDocService.findOne(+id);
+  findOne(
+    @Param('id') id: number
+  ): Promise<ApiResponse<ProjectDocResponseDto>> {
+    const data = this.projectDocService.findOne(id);
+    return new ApiResponse<ProjectDocResponseDto>(200, "조회", data);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProjectDocDto: UpdateProjectDocDto) {
-    return this.projectDocService.update(+id, updateProjectDocDto);
+  update(
+    @Param('id') id: number, 
+    @Body() updateProjectDocDto: UpdateProjectDocDto
+  ): Promise<ApiResponse<ProjectDocResponseDto>> {
+    const data = this.projectDocService.update(id, updateProjectDocDto);
+    return new ApiResponse<ProjectDocResponseDto>(200, "수정", data);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.projectDocService.remove(+id);
+  remove(
+    @Param('id') id: number
+  ): Promise<{ message: string }> {
+    const data = this.projectDocService.remove(id);
+    return { message: '삭제 성공' };
   }
 }
