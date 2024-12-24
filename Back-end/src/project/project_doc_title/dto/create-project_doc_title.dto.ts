14b2f2f776a75a857d9a6e37e9279cdc2df27e91
jhk <qwertyjhk11@naver.com>
2024-12-24 09:36:57 +0900

    추가: CreateProjectDocTitleDto 클래스에 pa_title_id 속성 추가 및 검증 규칙 정의

--- Back-end/src/project/project_doc_title/dto/create-project_doc_title.dto.ts
+++ Back-end/src/project/project_doc_title/dto/create-project_doc_title.dto.ts
@@ -1,11 +1,13 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString, Max, Min } from 'class-validator';
 
 export class CreateProjectDocTitleDto {
     @IsNotEmpty()
     @IsString()
     title: string;
-    
-    @IsNotEmpty()
-    @IsString()
-    description: string;
+
+    @IsNumber()
+    @IsOptional()
+    @Min(1)
+    @Max(10000)
+    pa_title_id?: number;
 }
