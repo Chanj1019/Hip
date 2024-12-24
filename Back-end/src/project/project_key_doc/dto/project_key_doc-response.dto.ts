4c17cd592b0331b2d647dbabf856146fdad2f854
jhk <qwertyjhk11@naver.com>
2024-12-24 10:49:09 +0900

    수정: ProjectKeyDocResponseDto 클래스에서 project_data 속성 제거

--- Back-end/src/project/project_key_doc/dto/project_key_doc-response.dto.ts
+++ Back-end/src/project/project_key_doc/dto/project_key_doc-response.dto.ts
@@ -6,13 +6,11 @@ export class ProjectKeyDocResponseDto {
     key_doc_title: string;
     key_doc_url: string;
     key_doc_category: string;
-    project_data: ProjectResponseDto;
 
     constructor(entity: ProjectKeyDoc) {
         this.key_doc_id = entity.key_doc_id;
         this.key_doc_title = entity.key_doc_title;
         this.key_doc_url = entity.key_doc_url;
         this.key_doc_category = entity.key_doc_category;
-        this.project_data = new ProjectResponseDto(entity.project);
     }
 }
