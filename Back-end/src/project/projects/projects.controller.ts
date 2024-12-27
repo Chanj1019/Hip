ce93868932021968fd4dcefc74961685e1843dfe
jhk <qwertyjhk11@naver.com>
2024-12-27 09:09:56 +0900

    추가: findOne 메서드에 student, admin, instructor 역할 추가 및 프로젝트 조회 기능 구현

--- Back-end/src/project/projects/projects.controller.ts
+++ Back-end/src/project/projects/projects.controller.ts
@@ -37,6 +37,15 @@ export class ProjectsController {
         return new ApiResponse<ProjectResponseDto[]>(200, "프로젝트 전체 조회", data);
     }
 
+    @Get(':id')
+    @Roles('student','admin','instructor')
+    async findOne(
+        @Param('id', ParseIntPipe) id: number
+    ): Promise<ApiResponse<ProjectResponseDto>> {
+        const data = await this.projectsService.findOne(id);
+        return new ApiResponse<ProjectResponseDto>(200, "프로젝트 조회", data);
+    }
+
     @Patch(':id/update')
     @Roles('instructor','admin','student')
     @UseGuards(ApprovedStudentGuard)
