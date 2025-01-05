5a874e5ea489b4dc7061775bcc833b2f7d01737d
jhk <qwertyjhk11@naver.com>
2025-01-05 21:37:09 +0900

    수정: project_doc_pa_title_id 다시 작성

--- Back-end/src/project/project_doc_title/dto/create-project_doc_title.dto.ts
+++ Back-end/src/project/project_doc_title/dto/create-project_doc_title.dto.ts
@@ -4,4 +4,10 @@ export class CreateProjectDocTitleDto {
     @IsNotEmpty()
     @IsString()
     project_doc_title: string;
+
+    @IsNumber()
+    @IsOptional()
+    @Min(1)
+    @Max(10000)
+    project_doc_pa_title_id?: number;
 }
